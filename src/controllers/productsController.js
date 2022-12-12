b0c2f24c8b541523997a8de3c7a9da5ad3c1e028
Danieli Jaqueline do Nascimento <djaqueline.nascimento@gmail.com>
2022-12-11 22:16:09 -0300

    altera productsController

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -4,13 +4,9 @@ module.exports = {
     listProducts: (req, res) => {
         const products = productsModel.products();
         return res.render('list-products', {  products });
-<<<<<<< HEAD
-    },
-=======
     }, 
 
     descriptionProduct: (req, res) => {
         res.render('product-description')
     } 
->>>>>>> dev-kaique
 }
