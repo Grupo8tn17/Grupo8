6317967c71e65fe22ead548a5756eef4f96c1f40
Nic0ras <nicollai2006@gmail.com>
2023-01-22 09:02:07 -0300

    alterações productsController

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,7 +2,7 @@ const productsModel = require('../models/productsModel')
 
 module.exports = {
     listProducts: (req, res) => {
-        const products = productsModel.products;
+        const products = productsModel.products();
         return res.render('list-products', {  products });
     }
 }
