82a18311ff71fdda23b48f51cdbdbdceea1f251b
Maria Lucilia Seraidarian <mluciliaserai@gmail.com>
2023-01-29 15:52:31 -0300

    Correção das funcionalidades do carrinho

--- public/javascripts/cart.js
+++ public/javascripts/cart.js
@@ -1,64 +1,172 @@
-const cartProduct = document.querySelectorAll(".cart-product");
-const removeIcon = document.querySelector(".cart-remove-icon");
-const addIcon = document.querySelector(".cart-add-icon");
-const quantity = document.querySelector(".quantity-product");
-const removeProduct = document.querySelectorAll(".cart-product button");
-var removeCart = document.getElementById("remove-product");
-const cartInvisible = document.querySelector(".cart-invisible");
-const quantidade = parseInt(quantity.value);
-var productsCart = document.querySelectorAll(".products-cart");
-let contador = parseInt(quantity.innerText);
-
-const cart = JSON.parse(localStorage.getItem('cart'))
-
-
-//adicionar quantidade através do botão "+"
-
-addIcon.addEventListener("click", function () {
-  let newCounter = parseInt(quantity.innerText);
-  if (newCounter >= 1) {
-   let valor = newCounter + 1
-    quantity.innerText = valor;    
-  }  
-  localStorage.setItem("cart", JSON.stringify(cart));
-});
-
-//remover quantidade através do botão "-"
-
-removeIcon.addEventListener("click", function () {
-  let counter = parseInt(quantity.innerText);
-  if (counter != 0 && counter > 1) {
-    let valor = newCounter - 1
-    quantity.innerText = valor;
-    atualizarCarrinho(valor, id);
+function subtractItem() {
+  console.log(this)
+  let id = this.closest("tr").id;
+  let elementQtd = document.querySelector('tr[id="' + id + '"] .quantity');
+  let elementPrice = document.querySelector('tr[id="' + id + '"] .price');
+  let elementSubtotal = document.querySelector('tr[id="' + id + '"] .subtotal');
+  let qtd = Number(elementQtd.innerText);
+  let price = elementPrice.innerText.replace(",", ".");
+  let subtotalItem = Number(elementSubtotal.innerText);
+  
+  if (qtd != 0 && qtd > 1) {
+    qtd -= 1;
+    subtotalItem = subtotal(qtd, price);
+    elementQtd.innerText = qtd;
+    elementSubtotal.innerText = subtotalItem.replace(".", ",");
   }
-  
-});
-
-//remover produto do carrinho pelo botão X
-
-removeProduct.addEventListener("click", function(){
-  //escrever código
-  
-  localStorage.setItem("cart", JSON.stringify(novoCart));
-})
- 
-
-//mostrar carrinho vazio:
-
-if(cart.length <= 0){
-  mostrar o carrinho vazio
+  updateItemQtd(id, qtd);
 }
 
+function addItem() {
+  let id = this.closest("tr").id;
+  let elementQtd = document.querySelector('tr[id="' + id + '"] .quantity');
+  let elementPrice = document.querySelector('tr[id="' + id + '"] .price');
+  let elementSubtotal = document.querySelector('tr[id="' + id + '"] .subtotal');
+  let qtd = Number(elementQtd.innerText);
+  let price = elementPrice.innerText.replace(",", ".");
+  let subtotalItem = Number(elementSubtotal.innerText);
 
-//somando o total do carrinho:
+  if (qtd >= 0) {
+    qtd += 1;
+    subtotalItem = subtotal(qtd, price);
+    elementQtd.innerText = qtd;
+    elementSubtotal.innerText = subtotalItem.replace(".", ",");
+  }
 
-let subtotal = 0;
-let desconto = 0;
-let frete = 0;
-let total = 0;
+  updateItemQtd(id, qtd);  
+}
 
-//em cada item pegar o subtotal
-subtotal += cart[i].price * cart[i].quantity
+function calcularTotal() {
+  const subtotais = document.querySelectorAll(".subtotal");
+  let resultado = 0;
+  subtotais.forEach((subtotal) => {
+    let preco = subtotal.innerText.replace(",", ".");
+    resultado += parseFloat(preco);
+  });
 
-total = subtotal - frete 
+  return resultado.toFixed(2).replace(".", ",");
+}
+
+function updateTotal() {
+  document.querySelector(".resultado").innerText = calcularTotal();
+}
+
+function toReal(price) {
+  return price.toFixed(2).replace(".", ",");
+}
+
+function subtotal(quantity, price) {
+  return toReal(Number(price * quantity));
+}
+
+function updateItemQtd(id, quantity) {
+  const productsCart = JSON.parse(localStorage.getItem("cart")) || [];
+  productsCart.forEach((item) => {
+    if (item.id == id) {
+      item.quantity = quantity;
+      localStorage.setItem("cart", JSON.stringify(productsCart));
+    }
+    updateTotal();
+  });
+  }
+
+function removeItem () {
+  let id = this.closest("tr").id;
+  let item = this.closest("tr");
+  item.remove();
+  let productsCart = JSON.parse(localStorage.getItem("cart")) || [];
+  productsCart = productsCart.filter((item) => item.id != id);
+  localStorage.setItem("cart", JSON.stringify(productsCart));
+  updateTotal();
+}
+
+function showEmptyCart () {
+  let cartEmpty = document.querySelector('.cart-empty')
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  if(cart.length == 0){
+    meuCarrinho.style.display = "none";
+    cartEmpty.classList.remove('cart-invisible');
+  };
+};
+
+function buildCart() {
+  //buscando o produto no localStorage
+  let objectCart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  
+  //selecionando o elemento/tag onde o produto será adicionado no HTML
+  var cartProduct = document.querySelector(".cart-product"); //tbody
+
+  //adicionando o novo produto ao HTML
+  for (var i = 0; i <= objectCart.length - 1; i++) {
+    cartProduct.innerHTML +=
+      '<tr id="' +
+      objectCart[i].id +
+      '" class="item-product">' +
+      '<td class="id" style="min-width: 7rem;">' +
+      objectCart[i].id +
+      "</td>" +
+      "<td>" +
+      '<div class="image-product" rowspan="2">' +
+      '<img src="' +
+      objectCart[i].img +
+      '">' +
+      "<span>" +
+      objectCart[i].title +
+      "</span>" +
+      "</div>" +
+      "</td>" +
+      '<td style="min-width: 7rem;"> R$ <span class="price">' +
+      toReal(objectCart[i].price) +
+      "</span></td>" +
+      '<td style="width: 10rem;"> ' +
+      "<div>" +
+      '<button class="btnAdd">+</button>' +
+      '<span class="quantity">' +
+      objectCart[i].quantity +
+      "</span>" +
+      '<button class="btnSubtract">-</button>' +
+      "</div>" +
+      "</td>" +
+      "<td>" +
+      "R$ <span class='subtotal'>" +
+      subtotal(objectCart[i].quantity, objectCart[i].price) +
+      "</span>" +
+      "</td>" +
+      '<td style="width: 8rem;">' +
+      '<button class="btnRemove"> X </button>' +
+      "</td>" +
+      "</tr>";
+  }
+};
+
+buildCart();
+
+const meuCarrinho = document.querySelector(".meu-carrinho"); //conteiner carrinho
+const itemProduct = document.querySelector(".item-product"); // linha do produto
+let quantity = document.querySelectorAll(".quantity"); // quantidade
+let qtd = parseInt(quantity.innerText);
+const id = document.querySelectorAll(".id");
+
+const btnAddList = document.querySelectorAll(".btnAdd"); //botão de acrescentar qtd
+
+btnAddList.forEach(function (btnAdd) {
+  btnAdd.addEventListener("click", addItem.bind(btnAdd));
+});
+
+const btnSubtractList = document.querySelectorAll(".btnSubtract"); //botão de diminuir qtd
+
+btnSubtractList.forEach(function (btnSubtract) {
+  btnSubtract.addEventListener("click", subtractItem.bind(btnSubtract));
+});
+
+const btnRemoveList = document.querySelectorAll(".btnRemove"); // botão de remover produto do carrinho
+
+btnRemoveList.forEach(function (btnRemove) {
+  console.log(btnRemove)
+  btnRemove.addEventListener("click", removeItem.bind(btnRemove));
+});
+
+updateTotal();
+
+showEmptyCart();
--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,5 +1,5 @@
 module.exports = {
     mostraCarrinho: (req, res) => {
-        res.render('cart', {css: ['style.css', 'cart.css'], js: ["productsCart.js"]})
+        res.render('cart', {css: ['style.css', 'cart.css'], js: ['cart.js']})
     }
 }
