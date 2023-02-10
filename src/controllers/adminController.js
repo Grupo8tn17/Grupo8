2fd9968ef7e9971e7a92cc97668e8ba4bc4af245
Maria Lucilia Seraidarian <mluciliaserai@gmail.com>
2023-02-10 19:24:33 -0300

    Inclusão de validações com o express-validation

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -3,66 +3,69 @@ const productsModel = require('../models/productsModel');
 const { validationResult } = require('express-validator');
 
 module.exports = {
-    indexAdmin: (req, res) => {
-        res.render('panelAdmin-home');
-    }, 
+  indexAdmin: (req, res) => {
+    res.render("panelAdmin-home");
+  },
 
-    adminProducts: (req, res) => {
-        const products = productsModel.products();
-        res.render('panelAdmin-addProducts', {products});
-    }, 
+  adminProducts: (req, res) => {
+    const products = productsModel.products();
+    res.render("panelAdmin-addProducts", { products });
+  },
 
-    addProducts: (req, res) => {
-        //inlcui array de erros
-        res.render('panelAdmin-add', { errors: {} });
-    },
+  addProducts: (req, res) => {
+    //inlcui array de erros
+    res.render("panelAdmin-add", { errors: {}, product: {} });
+  },
 
-    createProducts: (req, res) => {
-        //inclusão middleware de validacao:
-        let { errors } = validationResult(req);
-
+  createProducts: (req, res) => {
+    //inclusão middleware de validacao:
+    let { errors } = validationResult(req);
+    console.log(errors)
+   
     if (errors.length) {
-      const errosFormatados = {};
-      errors.forEach(erro => errosFormatados[erro.param] = erro.msg);
-
-      return res.render('panelAdmin-add', { errors: errosFormatados, servico: null });
+      const errorsFormated = {};
+      errors.forEach((error) => (errorsFormated[error.param] = error.msg));
+      
+      return res.render('panelAdmin-add', {
+        errors: errorsFormated,
+        product: req.body,
+      });
     }
-        productsModel.productsCreate(req);
-        res.redirect('/admin/products');
-    }, 
-    
-    deleteView: (req, res) => {
-        const { id } = req.params;
-        let product = null;
+    productsModel.productsCreate(req);
+    res.redirect('/admin/products');
+  },
 
-        if(id) {
-            product = productsModel.findProducts(id);
-        }
+  deleteView: (req, res) => {
+    const { id } = req.params;
+    let product = null;
 
-        return res.render('panelAdmin-delete', {product})
-        
-    },
-
-    deleteProducts: (req, res) => {
-        const {id}= req.params;
-        productsModel.deleteProduct(id);
-        return res.redirect('/admin/products');
-    }, 
-    
-    updateProducts: (req, res) => {
-        const { id } = req.params;
-        let product = null;
-
-        if(id) {
-            product = productsModel.findProducts(id);
-        }
-        return res.render('panelAdmin-updateProduct', {product})
-    },
-
-    update: (req, res) => {
-        const {id} = req.params;
-
-        productsModel.updateProducts(id, req.body);
-        return res.redirect('/admin/products');
+    if (id) {
+      product = productsModel.findProducts(id);
     }
-}
+
+    return res.render("panelAdmin-delete", { product });
+  },
+
+  deleteProducts: (req, res) => {
+    const { id } = req.params;
+    productsModel.deleteProduct(id);
+    return res.redirect("/admin/products");
+  },
+
+  updateProducts: (req, res) => {
+    const { id } = req.params;
+    let product = null;
+
+    if (id) {
+      product = productsModel.findProducts(id);
+    }
+    return res.render("panelAdmin-updateProduct", { product });
+  },
+
+  update: (req, res) => {
+    const { id } = req.params;
+
+    productsModel.updateProducts(id, req.body);
+    return res.redirect("/admin/products");
+  },
+};
