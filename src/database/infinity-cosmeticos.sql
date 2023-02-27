d5242ce2f34eb23b228ab9e7dfbbcb23f964abac
Maria Lucilia Seraidarian <mluciliaserai@gmail.com>
2023-02-26 22:41:17 -0300

    Inclui sql do banco de dados

--- src/database/infinity-cosmeticos.sql
+++ src/database/infinity-cosmeticos.sql
@@ -2,37 +2,6 @@ CREATE DATABASE infinity_cosmeticos;
 
 USE infinity_cosmeticos;
 
-CREATE TABLE produtos (
-	idprodutos INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL, 
-    titulo VARCHAR(255) NOT NULL, 
-    descricao LONGTEXT NOT NULL, 
-    valor DECIMAL(5,2) NOT NULL, 
-    quantidade INT NOT NULL, 
-    imagem VARCHAR(255) NOT NULL,
-    marca VARCHAR(150) NOT NULL, 
-    categorias_idcategorias INT UNSIGNED NOT NULL, 
-    carrinho_idcarrinho INT UNSIGNED NOT NULL,
-    FOREIGN KEY (categorias_idcategorias) REFERENCES categorias(idcategorias),
-    FOREIGN KEY (carrinho_idcarrinho) REFERENCES carrinho(idcarrinho)
-);
-
-CREATE TABLE categorias (
-	idcategorias INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    nome VARCHAR(45) NOT NULL
-);
-
-CREATE TABLE carrinho ( 
-	idcarrinho INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    valor_total DECIMAL(5,2) NOT NULL, 
-    quantidade INT NOT NULL
-);
-
-ALTER TABLE carrinho 
-	ADD COLUMN  usuarios_idusuarios INT UNSIGNED NOT NULL;
-    
-ALTER TABLE carrinho ADD CONSTRAINT usuarios_idusuarios
-FOREIGN KEY (usuarios_idusuarios) REFERENCES usuarios(idusuarios);
-
 CREATE TABLE usuarios (
 	idusuarios INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nome VARCHAR(150) NOT NULL, 
@@ -42,13 +11,58 @@ CREATE TABLE usuarios (
     documento_usuario INT(20) NOT NULL, 
     telefone INT(11) NOT NULL,
     data_nascimento DATE NOT NULL,
-    idadmin INT NOT NULL
+    idadmin INT NOT NULL,
+    foto_usuario VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE pedidos (
 	idpedidos INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
     codigo_pedido INT NOT NULL, 
     data_pedido DATE NOT NULL
+);
+
+CREATE TABLE categorias (
+	idcategorias INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nome VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE marcas (
+	idmarca INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nome VARCHAR(150) NOT NULL, 
+    cnpj INT(20) NOT NULL,
+    endereco VARCHAR(150) NOT NULL
+);
+
+CREATE TABLE produtos_ofertas (
+	idprodutos_ofertas INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    oferta TINYINT NOT NULL, 
+    valor_oferta DECIMAL(5,2) NOT NULL
+);
+
+CREATE TABLE carrinho ( 
+	idcarrinho INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    valor_total DECIMAL(5,2) NOT NULL, 
+    quantidade INT NOT NULL,
+    cupom_desconto VARCHAR(45),
+    usuarios_idusuarios INT UNSIGNED NOT NULL,
+    FOREIGN KEY (usuarios_idusuarios) REFERENCES usuarios(idusuarios)
+);
+
+CREATE TABLE produtos (
+	idprodutos INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL, 
+    titulo VARCHAR(255) NOT NULL, 
+    descricao LONGTEXT NOT NULL, 
+    valor DECIMAL(5,2) NOT NULL, 
+    quantidade INT NOT NULL, 
+    imagem VARCHAR(255) NOT NULL,
+    marca VARCHAR(150) NOT NULL, 
+    ativo TINYINT NOT NULL,
+    categorias_idcategorias INT UNSIGNED NOT NULL, 
+    carrinho_idcarrinho INT UNSIGNED NOT NULL,
+    marcas_idmarca INT UNSIGNED NOT NULL,
+    FOREIGN KEY (categorias_idcategorias) REFERENCES categorias(idcategorias),
+    FOREIGN KEY (carrinho_idcarrinho) REFERENCES carrinho(idcarrinho),
+    FOREIGN KEY (marcas_idmarca) REFERENCES marcas(idmarca)
 );
 
 CREATE TABLE historico_pedidos (
@@ -65,7 +79,8 @@ CREATE TABLE forma_pagamentos (
     carrinho_usuarios_idusuarios INT UNSIGNED NOT NULL,
     pedidos_idpedidos INT UNSIGNED NOT NULL,
     FOREIGN KEY (carrinho_idcarrinho) REFERENCES carrinho(idcarrinho),
-    FOREIGN KEY (carrinho_usuarios_idusuarios) REFERENCES carrinho(usuarios_idusuarios)
+    FOREIGN KEY (pedidos_idpedidos) REFERENCES pedidos(idpedidos),
+    FOREIGN KEY (carrinho_usuarios_idusuarios) REFERENCES carrinho(usuarios_idusuarios)    
 );
 
 CREATE TABLE enderecos (
@@ -82,23 +97,20 @@ CREATE TABLE enderecos (
     carrinho_idcarrinho INT UNSIGNED NOT NULL,
     carrinho_usuarios_idusuarios INT UNSIGNED NOT NULL,
     FOREIGN KEY (usuarios_idusuarios) REFERENCES usuarios(idusuarios),
-    FOREIGN KEY (carrinho_idcarrinho) REFERENCES carrinho(idcarrinho),
-    FOREIGN KEY (carrinho_usuarios_idusuarios) REFERENCES carrinho(usuarios_idusuarios)
+    FOREIGN KEY (carrinho_idcarrinho) REFERENCES carrinho(idcarrinho)
 );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+CREATE TABLE produtos_has_produtos_ofertas (
+	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    produtos_idprodutos INT UNSIGNED NOT NULL,
+    produtos_categorias_idcategorias INT UNSIGNED NOT NULL,
+    produtos_carrinho_idcarrinho INT UNSIGNED NOT NULL,
+    produtos_marcas_idmarca INT UNSIGNED NOT NULL,
+    produtos_ofertas_idprodutos_ofertas INT UNSIGNED NOT NULL,
+    FOREIGN KEY (produtos_idprodutos) REFERENCES produtos(idprodutos),
+    FOREIGN KEY (produtos_categorias_idcategorias) REFERENCES produtos(categorias_idcategorias),
+    FOREIGN KEY (produtos_carrinho_idcarrinho) REFERENCES produtos(carrinho_idcarrinho),
+    FOREIGN KEY (produtos_marcas_idmarca) REFERENCES produtos(marcas_idmarca),
+    FOREIGN KEY (produtos_ofertas_idprodutos_ofertas) REFERENCES produtos_ofertas(idprodutos_ofertas)
+);
 
