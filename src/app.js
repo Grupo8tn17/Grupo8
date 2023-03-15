20cced09a1f864a79783a843bd508bb775eace22
Maria Lucilia Seraidarian <mluciliaserai@gmail.com>
2023-03-15 18:29:41 -0300

    acesso API Via CEP

--- src/app.js
+++ src/app.js
@@ -26,6 +26,7 @@ app.use('/admin', adminRouter);
 app.use('/products', productsRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
